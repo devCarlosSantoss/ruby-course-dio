5bffed8aec216ecee69bb8fa85017bc339382284
devCarlosSantoss <carlosvinicius357890@gmail.com>
2023-06-11 17:49:18 -0300

    Initial commit

---
+++ desafio_1.rb
@@ -0,0 +1,7 @@
+print "Digite seu nome: "
+nome = gets.chomp
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp
+puts "Seu nome é #{nome} #{sobrenome}. Sua idade é #{idade}"
